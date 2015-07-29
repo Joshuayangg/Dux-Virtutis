91dd07e19a669e7804a0f15aed48976588888bcc
mdsassoon <Jeomdragon@gmail.com>
2015-07-29 13:48:44 -0700

    Dux 0.6

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ unitArr = [];
 unitCount = 0;
 //Initializing global variables
 window.onload = function(){
-  var game = new Game(500, 500);
+  var game = new Game(480, 480);
   game.spriteSheetWidth = 160;
   game.spriteSheetHeight = 32;
   game.fps = 15;
@@ -42,12 +42,12 @@ window.onload = function(){
     //Adding chilcren to root scene
   };
   function Block(x,y){
-  this.occupied = false;
-  this.xIndex = x;
-  this.yIndex = y;
-  this.xPos = this.xIndex*game.spriteWidth;
-  this.yPos = this.yIndex*game.spriteHeight;
-  this.unitIndex;
+    this.occupied = false;
+    this.xIndex = x;
+    this.yIndex = y;
+    this.xPos = this.xIndex*game.spriteWidth;
+    this.yPos = this.yIndex*game.spriteHeight;
+    this.unitIndex;
     this.toString = function(){
       return this.xIndex+","+this.yIndex;
     }
@@ -72,21 +72,22 @@ window.onload = function(){
 
   var unit = new Sprite(game.spriteWidth, game.spriteHeight);
   var setUnit = function(x,y){
-    unit.mov;
+    unit.movement = 3;
     unit.atk;
     unit.HP;
     unit.def;
     //Unit parameters
 
     unit.spriteOffset = 1;
-    unit.startBlock = grid[x][y];
-    unit.startBlock.occupied = true;
-    unit.startBlock.unitIndex = unitCount;
+    unit.canMove = true;
+    unit.currentBlock = grid[x][y];
+    unit.currentBlock.occupied = true;
+    unit.currentBlock.unitIndex = unitCount;
     unitCount++;
-    unit.x = unit.startBlock.xPos;
-    unit.y = unit.startBlock.yPos;
-    unit.xInt = unit.startBlock.xIndex;
-    unit.yInt = unit.startBlock.yIndex;
+    unit.x = unit.currentBlock.xPos;
+    unit.y = unit.currentBlock.yPos;
+    unit.xInt = unit.currentBlock.xIndex;
+    unit.yInt = unit.currentBlock.yIndex;
     unit.frame = unit.spriteOffset;
     unit.image = new Surface(game.spriteSheetWidth,game.spriteSheetHeight);
     unit.image.draw(game.assets['sprites.png']);
@@ -94,10 +95,72 @@ window.onload = function(){
     //Position and sprite variables
   }
   unit.move = function(x,y){
-    unit.moveTo(x*spriteWidth,y*spriteHeight);
-    unit.startBlock.occupied = false;
+    unit.moveTo(x*game.spriteWidth,y*game.spriteHeight);
+    unit.currentBlock.occupied = false;
+    var index = unit.currentBlock.unitIndex;
+    grid[x][y].unitIndex = index;
+    unit.currentBlock.unitIndex = null;
     grid[x][y].occupied = true;
+    unit.currentBlock = grid[x][y];
   }
+
+  /*var Soldier = new Sprite(game.spriteWidth, game.spriteHeight);
+  var RPG = new Sprite(game.spriteWidth, game.spriteHeight);
+  var Jeep = new Sprite(game.spriteWidth, game.spriteHeight);
+  var Tank = new Sprite(game.spriteWidth, game.spriteHeight);
+
+  function unitFactory(){}
+  //Skeleton factory
+  unitFactory.prototype.unitClass = Soldier;
+  //Defualt Class
+
+  unitFactory.prototype.createUnit = function (options){
+    //Factory method
+    switch(options.unitType){
+      case "Soldier":
+        this.unitClass = Soldier;
+        break;
+      case "RPG":
+        this.unitClass = RPG;
+        break;
+      case "Jeep":
+        this.unitClass = Jeep;
+        break;
+      case "Tank":
+        this.unitClass = Tank;
+        break;
+    }
+
+    return new this.unitClass(options);
+  
+  }
+  //Factory test
+  var factory = new unitFactory();
+  var soldier = factory.createUnit({unitType: 'Soldier'});*/
+
+  game.rootScene.addEventListener('touchend', function(e){
+    var x = Math.floor(e.x/game.spriteWidth);
+    var y = Math.floor(e.y/game.spriteHeight);
+
+    var moveClick = function(e){
+      var x1 = Math.floor(e.x/game.spriteWidth);
+      var y1 = Math.floor(e.y/game.spriteHeight);
+      if(Math.abs(x-x1)+Math.abs(y-y1)<=unit.movement){
+        if(!(grid[x1][y1]===grid[x][y])){
+          unitArr[grid[x][y].unitIndex].move(x1,y1);
+          unitArr[grid[x1][y1].unitIndex].canMove = false;
+        }
+      }
+    }
+
+    if(grid[x][y].occupied&&unitArr[grid[x][y].unitIndex].canMove){
+      game.rootScene.addEventListener('touchstart', moveClick);
+      game.rootScene.addEventListener('touchend', function(f){
+        game.rootScene.removeEventListener('touchstart', moveClick);
+      })
+    }
+    });
+
   game.onload = function(){
     setMaps();
     setUnit(3,6);
