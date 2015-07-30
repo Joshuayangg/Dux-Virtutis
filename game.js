a35216ea8f98ed98e4f83e2b4e2343b545b7b51b
mdsassoon <Jeomdragon@gmail.com>
2015-07-30 13:42:33 -0700

    Tyranny 0.7

--- game.js
+++ game.js
@@ -36,11 +36,17 @@ window.onload = function(){
   var setStage = function(){
     var stage = new Group();
     stage.addChild(map);
-    stage.addChild(unit);
     stage.addChild(foregroundMap);
     game.rootScene.addChild(stage);
     //Adding chilcren to root scene
   };
+  var setUnits = function(){
+    var troops = new Group();
+    for(var x = 0; x < unitArr.length; x++){
+      troops.addChild(unitArr[x])
+      game.rootScene.addChild(troops);
+    }
+  }
   function Block(x,y){
     this.occupied = false;
     this.xIndex = x;
@@ -69,74 +75,111 @@ window.onload = function(){
     }
   }
   //Initializing a grid of blocks
-
-  var unit = new Sprite(game.spriteWidth, game.spriteHeight);
-  var setUnit = function(x,y){
-    unit.movement = 3;
-    unit.atk;
-    unit.HP;
-    unit.def;
+  function unitObject(unitType){
+    this.unitType = unitType;
+    this.unit = new Sprite(game.spriteWidth, game.spriteHeight);
+    this.movement = 3;
+    this.isEnemy;
+    this.armored;
+    this.piercing;
+    this.personCost;
+    this.productionCost;
+    this.atk;
+    this.HP;
+    this.def;
     //Unit parameters
-
-    unit.spriteOffset = 1;
-    unit.canMove = true;
-    unit.currentBlock = grid[x][y];
-    unit.currentBlock.occupied = true;
-    unit.currentBlock.unitIndex = unitCount;
-    unitCount++;
-    unit.x = unit.currentBlock.xPos;
-    unit.y = unit.currentBlock.yPos;
-    unit.xInt = unit.currentBlock.xIndex;
-    unit.yInt = unit.currentBlock.yIndex;
-    unit.frame = unit.spriteOffset;
-    unit.image = new Surface(game.spriteSheetWidth,game.spriteSheetHeight);
-    unit.image.draw(game.assets['sprites.png']);
-    unitArr.push(unit);
-    //Position and sprite variables
+    this.setUnit = function(x,y){
+      this.unit.unitobject = this;
+      this.unit.spriteOffset = 1;
+      this.unit.canMove = true;
+      this.unit.currentBlock = grid[x][y];
+      this.unit.currentBlock.occupied = true;
+      this.unit.currentBlock.unitIndex = unitCount;
+      unitCount++;
+      this.unit.x = this.unit.currentBlock.xPos;
+      this.unit.y = this.unit.currentBlock.yPos;
+      this.unit.xInt = this.unit.currentBlock.xIndex;
+      this.unit.yInt = this.unit.currentBlock.yIndex;
+      this.unit.frame = this.unit.spriteOffset;
+      this.unit.image = new Surface(game.spriteSheetWidth,game.spriteSheetHeight);
+      this.unit.image.draw(game.assets['sprites.png']);
+      unitArr.push(this.unit);
+      //Position and sprite variables
+    }
+    this.unit.move = function(x,y){
+      this.moveTo(x*game.spriteWidth,y*game.spriteHeight);
+      this.currentBlock.occupied = false;
+      var index = this.currentBlock.unitIndex;
+      grid[x][y].unitIndex = index;
+      this.currentBlock.unitIndex = null;
+      grid[x][y].occupied = true;
+      this.currentBlock = grid[x][y];
+    }
   }
-  unit.move = function(x,y){
-    unit.moveTo(x*game.spriteWidth,y*game.spriteHeight);
-    unit.currentBlock.occupied = false;
-    var index = unit.currentBlock.unitIndex;
-    grid[x][y].unitIndex = index;
-    unit.currentBlock.unitIndex = null;
-    grid[x][y].occupied = true;
-    unit.currentBlock = grid[x][y];
+  
+  function buildingObject(buildingType){
+    this.buildingType = buildingType;
+    this.building = new Sprite(game.spriteWidth,game.spriteHeight);
+    this.setBuilding = function(x,y){
+      this.building.spriteOffset = 0;
+      this.building.currentBlock = grid[x][y];
+      this.building.currentBlock.occupied = true;
+      this.building.x = this.building.currentBlock.xPos;
+      this.building.y = this.building.currentBlock.yPos;
+      this.building.xInt = this.building.currentBlock.xIndex;
+      this.building.yInt = this.building.currentBlock.yIndex;
+      this.building.frame = this.building.spriteOffset;
+      this.building.image = new Surface(game.spriteSheetWidth,game.spriteSheetHeight);
+      this.building.image.draw(game.assets['sprites.png']);
+    }
   }
 
-  /*var Soldier = new Sprite(game.spriteWidth, game.spriteHeight);
-  var RPG = new Sprite(game.spriteWidth, game.spriteHeight);
-  var Jeep = new Sprite(game.spriteWidth, game.spriteHeight);
-  var Tank = new Sprite(game.spriteWidth, game.spriteHeight);
+  Soldier.prototype = unitObject;
+  function Soldier(){
+
+    this.HP = 20;
+    this.atk = 12;
+    this.def = 2;
+    this.movement = 3;
+    this.armored = false;
+    this.piercing = false;
+    this.personCost = 150;
+    this.productionCost = 100;
+  }
+
+  RPG.prototype = unitObject;
+  function RPG(){
+
+  }
+
+  Jeep.prototype = unitObject;
+  function Jeep(){
+
+  }
+
+  Tank.prototype = unitObject;
+  function Tank(){
+
+  }
 
   function unitFactory(){}
   //Skeleton factory
   unitFactory.prototype.unitClass = Soldier;
   //Defualt Class
 
-  unitFactory.prototype.createUnit = function (options){
+  unitFactory.prototype.createUnit = function (unitType){
     //Factory method
-    switch(options.unitType){
-      case "Soldier":
-        this.unitClass = Soldier;
-        break;
-      case "RPG":
-        this.unitClass = RPG;
-        break;
-      case "Jeep":
-        this.unitClass = Jeep;
-        break;
-      case "Tank":
-        this.unitClass = Tank;
-        break;
-    }
-
-    return new this.unitClass(options);
+    return new unitObject(unitType);
   
   }
-  //Factory test
-  var factory = new unitFactory();
-  var soldier = factory.createUnit({unitType: 'Soldier'});*/
+
+  var battle = function(){
+    if(grid[x1][y1].occupied&&spot2.isEnemy){
+      if(spot2.def-spot1.atk<0){
+        spot2.HP-=(spot2.def-spot1.atk);
+      }
+    }
+  }
 
   game.rootScene.addEventListener('touchend', function(e){
     var x = Math.floor(e.x/game.spriteWidth);
@@ -145,11 +188,17 @@ window.onload = function(){
     var moveClick = function(e){
       var x1 = Math.floor(e.x/game.spriteWidth);
       var y1 = Math.floor(e.y/game.spriteHeight);
-      if(Math.abs(x-x1)+Math.abs(y-y1)<=unit.movement){
-        if(!(grid[x1][y1]===grid[x][y])){
-          unitArr[grid[x][y].unitIndex].move(x1,y1);
-          unitArr[grid[x1][y1].unitIndex].canMove = false;
+      if(!grid[x1][y1].occupied){
+        var spot1 = unitArr[grid[x][y].unitIndex];
+        var spot2 = unitArr[grid[x1][y1].unitIndex];
+        if(Math.abs(x-x1)+Math.abs(y-y1)<=spot1.unitobject.movement){
+          if(!(grid[x1][y1]===grid[x][y])){
+            spot1.move(x1,y1);
+            unitArr[grid[x1][y1].unitIndex].canMove = false;
+          }
         }
+      }else if(grid[x1][y1].isEnemy){
+        battle();
       }
     }
 
@@ -163,22 +212,26 @@ window.onload = function(){
 
   game.onload = function(){
     setMaps();
-    setUnit(3,6);
     setStage();
+    //Factory test
+    var factory = new unitFactory();
+    var soldier = factory.createUnit({unitType: 'Soldier'});
+    var soldier1 = factory.createUnit({unitType: 'Soldier'});
+    //var soldier2 = factory.createUnit({unitType: 'Soldier'});
+    soldier.setUnit(5,5);
+    console.log(soldier.unit.x + ',' + soldier.unit.y);
+    console.log(soldier1.unit.x + ',' + soldier1.unit.y);
+    soldier1.setUnit(5,6);
+    console.log(soldier.unit.x + ',' + soldier.unit.y);
+    console.log(soldier1.unit.x + ',' + soldier1.unit.y);
+    //soldier2.setUnit(5,4);
+    setUnits();
   };
   game.start();
 };
 
 /*
-  var building = new Sprite(game.spriteWidth, game.spriteHeight);
-    var setBuilding = function(){
-    	building.spriteOffset = 5;
-    	building.startingX = 10;
-    	building.startingY = 10;
-    	building.x = building.startingX * game.spriteWidth;
-    	building.y = building.startingY * game.spriteHeight;
-    	}
-
+  
   var posConversion = function(x,y){
   	x = Math.floor(x/game.spriteWidth);
   	y = Math.floor(y/game.spriteHeight);
